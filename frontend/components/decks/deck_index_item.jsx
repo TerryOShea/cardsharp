90fc9d3b1116190ee61ab62afea08944e7dcc1e9
Terry O'Shea <terrykoshea@gmail.com>
2017-03-16 14:23:20 -0700

    'My Decks' looks nice

--- frontend/components/decks/deck_index_item.jsx
+++ frontend/components/decks/deck_index_item.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router';
 
+const progressRef = {
+  '0': 'zero-fifths',
+  '1': 'one-fifth',
+  '2': 'two-fifths',
+  '3': 'three-fifths',
+  '4': 'four-fifths',
+  '5': ''
+}
+
 const DeckIndexItem = ({ deck }) => {
   const catName = deck.category.split("_").join("-");
+  const progressName = `progress ${progressRef[deck.mastery.toString()]}`;
+
   return (
     <li>
       <Link to={`decks/${deck.id}`} className={`deck-widget ${catName}`}>
-        {deck.title}
+        <section className={progressName}></section>
       </Link>
+      {deck.title}
     </li>
   );
 };
