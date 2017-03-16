12e5315aa4462decca8ca8a8be9e71590a47848e
Terry O'Shea <terrykoshea@gmail.com>
2017-03-16 10:05:57 -0700

    add search link to header

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -5,10 +5,13 @@ import GreetingContainer from './greeting/greeting_container';
 const App = ({ children }) => (
   <div className="page-container">
     <header>
-      <Link to="/" className="header-title">
-        <img src="/assets/favicon.png" className="header-logo" />
-        <h1><span>Card</span>Sharp</h1>
-      </Link>
+      <nav className="header-left">
+        <Link to="/" className="header-title">
+          <img src="/assets/favicon.png" className="header-logo" />
+          <h1><span>Card</span>Sharp</h1>
+        </Link>
+        <a href="#"><i className="fa fa-search"></i> Decks</a>
+      </nav>
       <GreetingContainer />
     </header>
     {children}
