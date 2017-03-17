4e89d47b921b0555debdc40cf419d3d6de57fb75
Terry O'Shea <terrykoshea@gmail.com>
2017-03-17 15:46:48 -0700

    card flipping

--- frontend/components/card_show/card_show.jsx
+++ frontend/components/card_show/card_show.jsx
@@ -1,11 +1,33 @@
 import React from 'react';
 
-const CardShow = ({ card }) => {
-  return (
-    <div className="card-show-container">
-      <div className="card-show">{card.side_a}</div>
-    </div>
-  )
-};
+class CardShow extends React.Component {
+  constructor(props) {
+    super(props);
+    this.flipCard = this.flipCard.bind(this);
+    this.state = { flip: false };
+  }
+
+  flipCard() {
+    this.setState({ flip: !this.state.flip });
+  }
+
+  render() {
+    const { card } = this.props;
+    const flipClasses = `flip-container${this.state.flip ? " flip" : ""}`;
+
+    return (
+      <div className="card-show-container">
+        <div className={flipClasses} onClick={this.flipCard}>
+          <div className="flipper">
+            <div className="card-show front">{card.side_a}</div>
+            <div className="card-show back">{card.side_b}</div>
+          </div>
+        </div>
+
+        <button type="button" className="card-show-btn" onClick={this.flipCard}>Reveal Answer</button>
+      </div>
+    )
+  }
+}
 
 export default CardShow;
